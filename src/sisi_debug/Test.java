e9029280e7ebe2b4aae78a0aec990f3be4468941
unknown <playflute@icloud.com>
2020-11-09 21:50:37 +0800

    delete inner for loop

--- src/sisi_debug/Test.java
+++ src/sisi_debug/Test.java
@@ -8,11 +8,7 @@ public class Test {
 		System.out.println("≤Â»Î“ªæ‰ª∞");
 		for(int i=1;i<=5;i++)
 		{
-			for(int k=1;k<=i;k++)
-			{
-				System.out.print(k);
-				System.out.print(" ");
-			}
+
 			System.out.println();
 		}
 
