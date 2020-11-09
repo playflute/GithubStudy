f4ba9ea65bb643d7dbca91abe4ed72c07c78622a
unknown <playflute@icloud.com>
2020-11-09 21:48:22 +0800

    add a new statement

--- src/sisi_debug/Test.java
+++ src/sisi_debug/Test.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 
 public class Test {
 	public static void main(String[] args) {
+		System.out.println("≤Â»Î“ªæ‰ª∞");
 		for(int i=1;i<=5;i++)
 		{
 			for(int k=1;k<=i;k++)
