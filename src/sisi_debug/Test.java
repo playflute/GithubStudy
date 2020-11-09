3676777c3bc39e58b3c7e073e6575f9d431588c6
unknown <playflute@icloud.com>
2020-11-09 21:38:15 +0800

    My very first time to upload this project

---
+++ src/sisi_debug/Test.java
@@ -0,0 +1,20 @@
+package sisi_debug;
+
+import java.util.ArrayList;
+
+
+public class Test {
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++)
+		{
+			for(int k=1;k<=i;k++)
+			{
+				System.out.print(k);
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
